0f0a01543315b5f70d45f02c1b2a8eb6a97da260
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-05-28 13:04:39 +0000

    Add viewing tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -13,4 +13,8 @@ document.addEventListener("DOMContentLoaded", function() {
     // select initilization
     let selects = document.querySelectorAll('select');
     M.FormSelect.init(selects);
+
+    // collapsible initializataion
+    let collapsibles = document.querySelectorAll(".collapsible");
+    M.Collapsible.init(collapsibles);
 });
